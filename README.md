a22921c249a2f7f18b38ceb31aa7ef7dab3e6097
Hamida Shaik <HS0705>
2020-05-27 17:27:41 -0700

    Merge Sort

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Sorting-Algorithms
 
 ### This repo has list of all the important Sorting algorithms
+#### Merge Sort
---
+++ merge.py
@@ -0,0 +1,31 @@
+# Merge sort a list
+def merge_sort(nums):
+    if len(nums)>1:
+        mid=len(nums)//2
+        left=nums[:mid]
+        right=nums[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        left_pointer=right_pointer=sorted_pointer=0
+        while(left_pointer<len(left) and right_pointer<len(right)):
+            if left[left_pointer]<right[right_pointer]:
+                nums[sorted_pointer]=left[left_pointer]
+                left_pointer += 1
+            else:
+                nums[sorted_pointer]=right[right_pointer]
+                right_pointer += 1
+            sorted_pointer += 1
+        if len(left):
+            while(left_pointer<len(left)):
+                nums[sorted_pointer]=left[left_pointer]
+                left_pointer += 1
+                sorted_pointer += 1
+        if len(right):
+            while(right_pointer<len(right)):
+                nums[sorted_pointer]=right[right_pointer]
+                right_pointer += 1
+                sorted_pointer += 1
+        return nums
+    else:
+        return
+print(merge_sort([23,11,10,5,9,7]))
